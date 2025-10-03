1c18ad4c3bde0b86d1c01d3d7043f96eada9b994
Puspak29 <samantapuspak@gmail.com>
2025-10-03 19:34:44 +0530

    feat: add user profile router and functionality via jwt

--- routers/user.js
+++ routers/user.js
@@ -1,9 +1,11 @@
 // Importing required modules
 const express = require("express"); // Express framework for building server
 const router = express.Router(); // Creating new router instance
+const middleware = require("../middleware");
 
-const { userDetails } = require("../controllers/user"); // User controller functions
+const { userDetails, getUserProfile } = require("../controllers/user"); // User controller functions
 
+router.get("/user/profile", middleware, getUserProfile);
 router.get("/user/:id", userDetails); // Route to get user details (GET /user/:id)
 
 // Exporting the router for use in other files
